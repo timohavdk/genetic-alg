63adfd529b14caf582f7180d9521814f7c15dc12
timohavdk <timoha-vdk@yandex.ru>
2024-01-16 13:27:44 +1000

    somefiles

--- gen.py
+++ gen.py
@@ -1,27 +1,29 @@
 from parse import parse_from_txt
 from developer_chromosome import DeveloperChromosome
+from k_point_crossover import k_point_crossover
 from task import Task
 import random
 import matplotlib.pyplot as plt
 
 # константы задачи
-ONE_MAX_LENGTH = 1000   # длина подлежащему оптимизации гену
-FILE_NAME = 'input.txt' # имя файла для конфигурации
+ONE_MAX_LENGTH = 1000    # длина подлежащему оптимизации гену
+FILE_NAME = 'input.txt'  # имя файла для конфигурации
 
 # константы генетического алгоритма
-POPULATION_SIZE = 400   # количество индивидуумов в популяции
-P_CROSSOVER = 1       # вероятность скрещивания
-P_MUTATION = 1        # вероятность мутации индивидуума
-MAX_GENERATIONS = 200    # максимальное количество поколений
+POPULATION_SIZE = 50     # количество индивидуумов в популяции
+P_CROSSOVER = 0.9        # вероятность скрещивания
+P_MUTATION = 0.1         # вероятность мутации индивидуума
+CROSS_INTERVAL_COUNT = 2 # Количсетво интервалов для скрещивания
+MAX_GENERATIONS = 100    # максимальное количество поколений
 
 RANDOM_SEED = 42
 random.seed(RANDOM_SEED)
 
 result = parse_from_txt(FILE_NAME)
-
+ONE_MAX_LENGTH = result.n_task
 tasks = []
 
-for i in range(result.n_task):
+for i in range(ONE_MAX_LENGTH):
     task = Task(result.task_difficult[i], result.task_timing[i])
     tasks.append(task)
 
@@ -35,21 +37,18 @@ class Individual(list):
         self.fitness = FitnessMax()
 
 def oneMaxFitness(individual):
-    # max_time = 0
-    # for item in individual:
-    #     if (item.timing_for_task > max_time):
-    #         max_time = item.timing_for_task
-    all_time = 0
+    max_time = 0
     for item in individual:
-        all_time += item.timing_for_task
+        if (item.timing_for_task > max_time):
+            max_time = item.timing_for_task
 
-    return ((1000000) / all_time), # кортеж
+    return 100000 / max_time, # кортеж
 
 def individualCreator():
     developer_index = random.randint(1, result.n_developers)
     developer_cof = result.developers_cof[developer_index - 1] 
     
-    return Individual([DeveloperChromosome(developer_index, developer_cof, tasks[i].time * developer_cof[tasks[i].difficult - 1], tasks[i]) for i in range(result.n_task)])
+    return Individual([DeveloperChromosome(developer_index, developer_cof, tasks[i].time * developer_cof[tasks[i].difficult - 1], tasks[i]) for i in range(ONE_MAX_LENGTH)])
 
 def populationCreator(n = 0):
     return list([individualCreator() for i in range(n)])
@@ -77,7 +76,7 @@ def selTournament(population, p_len):
         while i1 == i2 or i1 == i3 or i2 == i3:
             i1, i2, i3 = random.randint(0, p_len-1), random.randint(0, p_len-1), random.randint(0, p_len-1)
 
-        offspring.append(min([population[i1], population[i2], population[i3]], key=lambda ind: ind.fitness.values[0]))
+        offspring.append(max([population[i1], population[i2], population[i3]], key=lambda ind: ind.fitness.values[0]))
 
     return offspring
 
@@ -97,7 +96,6 @@ def mutChangeChrom(mutant, indpb=0.01):
 
 fitnessValues = [individual.fitness.values[0] for individual in population]
 
-#max(fitnessValues) < ONE_MAX_LENGTH and 
 while generationCounter < MAX_GENERATIONS:
     generationCounter += 1
     offspring = selTournament(population, len(population))
@@ -105,11 +103,12 @@ while generationCounter < MAX_GENERATIONS:
 
     for child1, child2 in zip(offspring[::2], offspring[1::2]):
         if random.random() < P_CROSSOVER:
-            cxOnePoint(child1, child2)
+            interval = int(ONE_MAX_LENGTH / CROSS_INTERVAL_COUNT)
+            child1, child2 = k_point_crossover(child1, child2, [random.randint((interval * i) + 1, (interval * (i + 1)) - -1) for i in range(0, CROSS_INTERVAL_COUNT)])
 
     for mutant in offspring:
         if random.random() < P_MUTATION:
-            mutChangeChrom(mutant, 0.1) # , indpb=1.0/ONE_MAX_LENGTH
+            mutChangeChrom(mutant, 1 / ONE_MAX_LENGTH)
 
     freshFitnessValues = list(map(oneMaxFitness, offspring))
     for individual, fitnessValue in zip(offspring, freshFitnessValues):
@@ -131,7 +130,11 @@ while generationCounter < MAX_GENERATIONS:
     data = []
     for i in range(0, len(best_ind)):
         data.append(best_ind[i].number)
-    print("Лучший время выполенния индивидуум = ", 1000000 / maxFitness, "\n")
+    all_time = 0
+    for item in range(0, len(best_ind)):
+        all_time += best_ind[item].timing_for_task
+    print("Score = ", 1000 / all_time, "\n")
+    print("Лучший индивидуум = ", data, "\n")
 
 plt.plot(maxFitnessValues, color='red')
 plt.plot(meanFitnessValues, color='green')
---
+++ k_point_crossover.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def k_point_crossover(a: np.ndarray, b: np.ndarray, points: np.ndarray):
+    temp_1 = a
+    temp_2 = b
+    
+    if 1 == len(points) or 2 == len(points):
+        endindx = len(temp_1) if 1 == len(points) else points[1]
+        startind = points[0] + 1
+        
+        temp_1[startind:endindx], temp_2[startind:endindx] = temp_2[startind:endindx], temp_1[startind:endindx]
+        
+        return temp_1, temp_2
+    
+    for i in range(0, len(points), 1):
+        curr_point = 0
+        last_point = 0
+            
+        if i != 0:
+            curr_point = points[i]
+            last_point = points[i - 1] + 1
+        
+        if (i == len(points) - 1):
+            if i % 2 != 0:
+                temp_1[last_point:curr_point], temp_2[last_point:curr_point] = temp_2[last_point:curr_point], temp_1[last_point:curr_point]
+            else:
+                temp_1[curr_point:], temp_2[curr_point:] = temp_2[curr_point:], temp_1[curr_point:]
+        if i % 2 != 0:
+            temp_1[last_point:curr_point], temp_2[last_point:curr_point] = temp_2[last_point:curr_point], temp_1[last_point:curr_point]
+    
+    return temp_1, temp_2
+
+result, result1 = k_point_crossover([1, 2, 3, 4, 5, 6, 9], [6, 5, 4, 3, 2, 1, 3], [1, 3])
+
+print("Res: ", result)
+print("Res1: ", result1)
